d8dfc8c5e65d2b4a12e4770e23f5abe5b8912061
Antonis Tsiapaliokas <antonis.tsiapaliokas@mykolab.com>
2026-05-25 12:36:03 +0100

    Implement CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,11 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests/visual',
   snapshotDir: './tests/visual/__screenshots__',
+  expect: {
+    toHaveScreenshot: {
+      maxDiffPixelRatio: 0.02
+    }
+  },
   use: {
     ...devices['Desktop Chrome'],
     viewport: {
